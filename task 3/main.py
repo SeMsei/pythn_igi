8cca110e5548e7f97d03c164caf8592c4be75c34
SeMsei <fomichevsjiy@gmail.com>
2023-02-18 14:54:54 +0300

    added third task

---
+++ task 3/main.py
@@ -0,0 +1,7 @@
+import utilities
+
+lst = input("Enter list items: ").split(' ')
+
+out_lst = utilities.get_even_numbers(lst)
+
+print(out_lst)
---
+++ task 3/utilities.py
@@ -0,0 +1,6 @@
+def get_even_numbers(lst):
+    lst_out = []
+    for item in lst:
+        if (int(item) % 2 == 0):
+            lst_out.append(item)
+    return lst_out
