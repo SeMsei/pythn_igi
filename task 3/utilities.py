87a76b6f4dd856325ac009408293386d3e621382
SeMsei <fomichevsjiy@gmail.com>
2023-02-20 00:03:49 +0300

    changed algorithm in 3 task

--- task 3/utilities.py
+++ task 3/utilities.py
@@ -1,8 +1,5 @@
 def get_even_numbers(lst):
-    lst_out = []
     
-    for item in lst:
-        if (int(item) % 2 == 0):
-            lst_out.append(item)
+    lst_out = [item for item in lst if int(item)%2==0]
             
     return lst_out
