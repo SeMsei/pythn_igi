810ee78486932ac3b8b0bc419ffa9a5a8cdd619a
SeMsei <fomichevsjiy@gmail.com>
2023-02-19 21:23:58 +0300

    changed scripts to get args from cmd

--- task 2/main.py
+++ task 2/main.py
@@ -1,8 +1,15 @@
 import utilities
+import sys
 
-a = int(input("input first number: "))
-b = int(input("input second number: "))
-opr = input("input operation (mul/sub/div/sum): ")
+#//a = int(input("input first number: "))
+#b = int(input("input second number: "))
+#opr = input("input operation (mul/sub/div/sum): ")
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+opr = sys.argv[3]
+
+print(a, b, opr)
 
 try:
     res = utilities.calc_two_num(a, b, opr)        
