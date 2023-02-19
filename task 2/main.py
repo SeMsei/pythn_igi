dc4f01dc3a3af9416d345fad5b7d5f29c27bcedc
SeMsei <fomichevsjiy@gmail.com>
2023-02-20 00:14:52 +0300

    changed input way and added try/catch

--- task 2/main.py
+++ task 2/main.py
@@ -1,15 +1,13 @@
 import utilities
 import sys
-
-#//a = int(input("input first number: "))
-#b = int(input("input second number: "))
-#opr = input("input operation (mul/sub/div/sum): ")
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-opr = sys.argv[3]
-
-print(a, b, opr)
+import os
+try:
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    opr = input("input operation (mul/sub/div/sum): ")
+except:
+    print('wrong input')
+    exit()
 
 try:
     res = utilities.calc_two_num(a, b, opr)        
