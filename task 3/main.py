329f85686af60563c9106820c721d204ee0788b9
SeMsei <fomichevsjiy@gmail.com>
2023-02-20 00:14:52 +0300

    changed input way and added try/catch

--- task 3/main.py
+++ task 3/main.py
@@ -1,6 +1,10 @@
 import utilities
 import sys
 
-lst = sys.argv[1:]
-out_lst = utilities.get_even_numbers(lst)
-print(out_lst)
+lst = input("input list of numbers: ").split(' ')
+
+try:
+    out_lst = utilities.get_even_numbers(lst)
+    print(out_lst)
+except:
+    print('wrong input')
