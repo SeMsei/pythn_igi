1e93e5d26a55c54595a474175b8b0e5bca63bfa4
SeMsei <fomichevsjiy@gmail.com>
2023-02-23 00:38:35 +0300

    changed output type in 3 task

--- Lab_1/task 3/utilities.py
+++ Lab_1/task 3/utilities.py
@@ -1,5 +1,5 @@
 def get_even_numbers(lst):
     
-    lst_out = [item for item in lst if int(item)%2==0]
+    lst_out = [int(item) for item in lst if int(item)%2==0]
             
     return lst_out
