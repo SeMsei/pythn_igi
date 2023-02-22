af07e08b21065a2b6684bb473aabc3f033c61ba7
SeMsei <fomichevsjiy@gmail.com>
2023-02-22 22:15:37 +0300

    changed variables names

--- Lab_1/task 2/main.py
+++ Lab_1/task 2/main.py
@@ -2,15 +2,15 @@ import utilities
 import sys
 import os
 try:
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
+    num1 = int(input("input first number: "))
+    num2 = int(input("input second number: "))
     opr = input("input operation (mul/sub/div/sum): ")
 except:
     print('wrong input')
     exit()
 
 try:
-    res = utilities.calc_two_num(a, b, opr)        
+    res = utilities.calc_two_num(num1, num2, opr)        
     print(res)
 except Exception as e:
     print(e)
