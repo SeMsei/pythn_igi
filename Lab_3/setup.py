63ebc6dd1a57569d8d9f65dd197d0f4ca956d457
SeMsei <fomichevsjiy@gmail.com>
2023-05-05 21:00:08 +0300

    added files to create module

---
+++ Lab_3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="serializer_for_igi",
+    version="1.01",
+    description="library for python serialization as lab",
+    url="https://github.com/SeMsei/pythn_igi/tree/lab3/Lab_3",
+    author="Denis Fomichevskiy",
+    author_email="fomichevsjiy@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    packages=["serializers/json", "serializers/dict_serializer", "serializers/xml"],
+    include_package_data=True
+)
