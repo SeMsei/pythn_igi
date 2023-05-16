f0f0605f16ef8c80cc772df9aca0b72c9bf72b8f
SeMsei <fomichevsjiy@gmail.com>
2023-05-16 22:03:44 +0300

    added client model

--- Lab_4/bookshop/shop/models.py
+++ Lab_4/bookshop/shop/models.py
@@ -62,6 +62,23 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Client(models.Model) :
+
+    first_name = models.CharField(max_length=200,
+                                help_text='Enter first name')
+    last_name = models.CharField(max_length=200,
+                                help_text='Enter last name')
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=50,
+                                    help_text='Enter phone number')
+    
+    def get_absolute_url(self):
+        return reverse('client-detail', args=[str(self.id)])
+
+    def __str__(self) :
+        return '{0}, {1}'.format(self.first_name, self.last_name) 
 '''
 class BookInstance(models.Model):
     uniqueId = models.SlugField(max_length=200, db_index=True)
