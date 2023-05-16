4f4c2a976f4f4126e40ec8293e9265af43e57be9
SeMsei <fomichevsjiy@gmail.com>
2023-05-16 19:41:55 +0300

    added cart

---
+++ Lab_4/bookshop/cart/cart.py
@@ -0,0 +1,79 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Book
+
+
+class Cart(object):
+
+    def __init__(self, request):
+        """
+        Инициализируем корзину
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # save an empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def add(self, book, quantity=1, update_quantity=False):
+        """
+        Добавить продукт в корзину или обновить его количество.
+        """
+        book_id = str(book.id)
+        if book_id not in self.cart:
+            self.cart[book_id] = {'quantity': 0,
+                                    'price': str(book.cost)}
+        if update_quantity:
+            self.cart[book_id]['quantity'] = quantity
+        else:
+            self.cart[book_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # Обновление сессии cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # Отметить сеанс как "измененный", чтобы убедиться, что он сохранен
+        self.session.modified = True
+
+    def remove(self, book):
+        """
+        Удаление товара из корзины.
+        """
+        book_id = str(book.id)
+        if book_id in self.cart:
+            del self.cart[book_id]
+            self.save()
+
+    def __iter__(self):
+        """
+        Перебор элементов в корзине и получение продуктов из базы данных.
+        """
+        book_ids = self.cart.keys()
+        # получение объектов product и добавление их в корзину
+        books = Book.objects.filter(id__in=book_ids)
+        for book in books:
+            self.cart[str(book.id)]['book'] = book
+
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Подсчет всех товаров в корзине.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        """
+        Подсчет стоимости товаров в корзине.
+        """
+        return sum(Decimal(item['price']) * item['quantity'] for item in
+                self.cart.values())
+    
+    def clear(self):
+        # удаление корзины из сессии
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
---
+++ Lab_4/bookshop/cart/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Book
+from .cart import Cart
+from .forms import CartAddBookForm
+
+
+@require_POST
+def cart_add(request, book_id):
+    cart = Cart(request)
+    book = get_object_or_404(Book, id=book_id)
+    form = CartAddBookForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(book=book,
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
+def cart_remove(request, book_id):
+    cart = Cart(request)
+    book = get_object_or_404(Book, id=book_id)
+    cart.remove(book)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
