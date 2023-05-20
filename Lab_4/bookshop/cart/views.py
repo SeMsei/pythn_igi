95a64dc534ebb02e50eab2fefe43b991c6431460
SeMsei <fomichevsjiy@gmail.com>
2023-05-20 11:03:08 +0300

    updated cart

--- Lab_4/bookshop/cart/views.py
+++ Lab_4/bookshop/cart/views.py
@@ -3,10 +3,13 @@ from django.views.decorators.http import require_POST
 from shop.models import Book
 from .cart import Cart
 from .forms import CartAddBookForm
-
+from django.core.exceptions import PermissionDenied
 
 @require_POST
 def cart_add(request, book_id):
+    if not request.user.is_authenticated:
+        raise PermissionDenied("Net dostupa")
+
     cart = Cart(request)
     book = get_object_or_404(Book, id=book_id)
     form = CartAddBookForm(request.POST)
@@ -18,11 +21,16 @@ def cart_add(request, book_id):
     return redirect('cart:cart_detail')
 
 def cart_remove(request, book_id):
+    if not request.user.is_authenticated:
+        raise PermissionDenied("Net dostupa")
+
     cart = Cart(request)
     book = get_object_or_404(Book, id=book_id)
     cart.remove(book)
     return redirect('cart:cart_detail')
 
 def cart_detail(request):
+    if not request.user.is_authenticated:
+        raise PermissionDenied("Net dostupa")
     cart = Cart(request)
     return render(request, 'cart/detail.html', {'cart': cart})
