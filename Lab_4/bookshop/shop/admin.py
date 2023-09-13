ee3088157496b7a613a24a91575ffc5a7e487de4
SeMsei <fomichevsjiy@gmail.com>
2023-09-14 02:05:54 +0300

    updated admin panel for the 1st lab

--- Lab_4/bookshop/shop/admin.py
+++ Lab_4/bookshop/shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Book, Author, Genre, Language, Client
+from .models import Book, Author, Genre, Language, Client, History, \
+                    Article, Adversisment, Partner, Vacancy, FAQ, WorkerPosition, \
+                    Comment
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
@@ -24,6 +26,36 @@ class BookAdmin(admin.ModelAdmin):
 class ClientAdmin(admin.ModelAdmin) :
     list_display = ['first_name', 'last_name', 'date_of_birth',
                     'email', 'phone_number']
+    
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ['year', 'description']
 
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['date', 'title', 'short', 'full', 'author_art', 'img']
 
+@admin.register(Adversisment)
+class AdversismentAdmin(admin.ModelAdmin):
+    list_display = ['text', 'img', 'link', 'company_name']
+
+@admin.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'img', 'link']
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'salary']
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ['question', 'date', 'answer']
+
+@admin.register(WorkerPosition)
+class WorkerPositionAdmin(admin.ModelAdmin):
+    list_display = ['worker', 'description', 'img']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['date', 'mark', 'author', 'text']
     
