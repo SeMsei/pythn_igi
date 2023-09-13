e558c11bca68cd0a7977195d6967af47978e11a7
SeMsei <fomichevsjiy@gmail.com>
2023-09-14 02:06:20 +0300

    updated urls for the 1st lab

--- Lab_4/bookshop/shop/urls.py
+++ Lab_4/bookshop/shop/urls.py
@@ -7,11 +7,24 @@ urlpatterns = [
     path('<int:id>', views.book_detail,
          name='book_detail'),
     path("create/", views.book_create),
+    path('about/', views.about_company),
+    path('privacy/', views.privacy),
+    path('main/', views.main),
+    path('articles/', views.articles),
+    path('articles/<int:id>/', views.full_article),
+    path('vacancies/', views.vacancies),
+    path('workers/', views.workers_list),
+    path('coupons/', views.coupons_list),
+    path('comments/', views.comments_list),
+    path('comments/create/', views.comment_create),
+    path('faq/', views.faqs),
+    path('faq/<int:id>/', views.faqs_full),
     path("edit/<int:id>/", views.book_edit),
     path("delete/<int:id>/", views.book_delete),
     path('', views.book_list, name='book_list'),
     path('<str:book_genre_name>/', views.book_list,
          name='book_list_by_genre'
          ),
+     
     
 ]
