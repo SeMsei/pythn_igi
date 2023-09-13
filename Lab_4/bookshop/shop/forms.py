e80367917df2456107fe0c711cb95c0b88c6f6dc
SeMsei <fomichevsjiy@gmail.com>
2023-09-14 02:05:32 +0300

    added form for the 1st lab

--- Lab_4/bookshop/shop/forms.py
+++ Lab_4/bookshop/shop/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Book
+from .models import Book, Comment, INT_CHOICES
 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'summary', 'imprint', 'image', 'ISBN', 'genre', 'language','cost']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text', 'mark']
+        widgets = {'mark': forms.Select(choices=INT_CHOICES),
+                   'text': forms.Textarea(attrs={'spellcheck': 'true', 'dir': 'auto'})}
